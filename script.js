459ce6ad58639912d94d3cf6b4d40d82c2cfb669
Emmanuel Puyos Jr <emmanuelpuyosjr@gmail.com>
2024-01-26 22:23:33 +0800

    final tweaks??

--- script.js
+++ script.js
@@ -1,6 +1,12 @@
 function toggleMenu() {
-    const menu = document.querySelector(".menu-links");
-    const icon = document.querySelector(".hamburger-icon");
-    menu.classList.toggle("open");
-    icon.classList.toggle("open");
+  const menu = document.querySelector(".menu-links");
+  const icon = document.querySelector(".hamburger-icon");
+  const navigation = document.querySelector(".primary-navigation");
+  const navigationHeight = navigation.offsetHeight;
+  document.documentElement.style.setProperty(
+    "--scroll-padding",
+    navigationHeight + "px"
+  );
+  menu.classList.toggle("open");
+  icon.classList.toggle("open");
 }
