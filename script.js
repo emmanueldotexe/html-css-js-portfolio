625c52d3099889b1778298382744f531cbac763e
Emmanuel Puyos Jr <emmanuelpuyosjr@gmail.com>
2024-01-26 22:37:20 +0800

    fixed hamburger menu

--- script.js
+++ script.js
@@ -1,12 +1,14 @@
 function toggleMenu() {
   const menu = document.querySelector(".menu-links");
   const icon = document.querySelector(".hamburger-icon");
-  const navigation = document.querySelector(".primary-navigation");
-  const navigationHeight = navigation.offsetHeight;
-  document.documentElement.style.setProperty(
-    "--scroll-padding",
-    navigationHeight + "px"
-  );
+
   menu.classList.toggle("open");
   icon.classList.toggle("open");
 }
+
+const navigation = document.querySelector(".primary-navigation");
+const navigationHeight = navigation.offsetHeight;
+document.documentElement.style.setProperty(
+  "--scroll-padding",
+  navigationHeight + "px"
+);
